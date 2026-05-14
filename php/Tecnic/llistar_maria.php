b3631e8925ebc61b28ff8ed3d422083fa4cdb0fe
a25jawmohbou-code <a25jawmohbou@inspedralbes.cat>
2026-05-14 09:40:59 +0200

    accents i errors

--- php/Tecnic/llistar_maria.php
+++ php/Tecnic/llistar_maria.php
@@ -23,7 +23,7 @@ $incidencies = $stmt->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Maria Lopez</title>
+    <title>María López</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <style>
